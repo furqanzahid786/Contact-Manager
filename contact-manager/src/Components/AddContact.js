86c7fb107b1c3967e3acbcd0846050420d3d12fd
furqanzahid786 <furqanzahid30@gmail.com>
2024-05-04 22:53:16 +0500

    First Commit

---
+++ contact-manager/src/Components/AddContact.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class AddContact extends React.Component
+{
+    render()
+    {
+        return(
+           <div>
+            <h3>Add Contact</h3>
+            <form>
+                    <div className='field'>
+                        <label>Name: </label>
+                        <input type='text' name='name' placeholder='Enter your name'/>
+                    </div>
+
+                    <div className='field'>
+                    <label>Email: </label>
+                        <input type='text' name='email' placeholder='Enter your email'/> 
+                    </div>
+                    <button className="ui button blue">Add</button>
+           </form>
+        </div>
+        );
+    }
+}
+
+export default AddContact;
